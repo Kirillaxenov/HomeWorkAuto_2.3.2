278681f7d80308c7a42894e1294af8b6916c9976
Kirill <kaxyonov@gmail.com>
2023-06-26 20:01:21 +0300

    initial commit

---
+++ src/test/java/AuthTest.java
@@ -0,0 +1,73 @@
+
+import org.junit.jupiter.api.*;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static ru.netology.DataGenerator.*;
+import static ru.netology.DataGenerator.Registration.getRegisteredUser;
+import static ru.netology.DataGenerator.Registration.getUser;
+
+
+class AuthTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void SuccessfulRegistered() {
+        var registeredUser = getRegisteredUser("active");
+        $("[data-test-id=login] input").setValue(registeredUser.getLogin());
+        $("[data-test-id=password] input").setValue(registeredUser.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $(byText("Личный кабинет")).shouldBe(visible);
+    }
+
+    @Test
+    void errorNotRegisteredUser() {
+        var notRegisteredUser = getUser("active");
+        $("[data-test-id=login] input").setValue(notRegisteredUser.getLogin());
+        $("[data-test-id=password] input").setValue(notRegisteredUser.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $(byText("Ошибка")).shouldBe(visible, Duration.ofSeconds(10));
+        $(withText("Неверно указан логин или пароль")).shouldBe(visible, Duration.ofSeconds(10));
+    }
+
+    @Test
+    void blockedUser() {
+        var blockedUser = getRegisteredUser("blocked");
+        $("[data-test-id=login] input").setValue(blockedUser.getLogin());
+        $("[data-test-id=password] input").setValue(blockedUser.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $(byText("Ошибка")).shouldBe(visible, Duration.ofSeconds(10));
+        $(withText("Пользователь заблокирован")).shouldBe(visible, Duration.ofSeconds(10));
+    }
+
+    @Test
+    void WrongLogin() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongLogin = login();
+        $("[data-test-id=login] input").setValue(wrongLogin);
+        $("[data-test-id=password] input").setValue(registeredUser.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $(byText("Ошибка")).shouldBe(visible, Duration.ofSeconds(10));
+        $(withText("Неверно указан логин или пароль")).shouldBe(visible, Duration.ofSeconds(10));
+    }
+
+    @Test
+    void WrongPassword() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongPassword = password();
+        $("[data-test-id=login] input").setValue(registeredUser.getLogin());
+        $("[data-test-id=password] input").setValue(wrongPassword);
+        $$("button").find(exactText("Продолжить")).click();
+        $(byText("Ошибка")).shouldBe(visible, Duration.ofSeconds(10));
+        $(withText("Неверно указан логин или пароль")).shouldBe(visible, Duration.ofSeconds(10));
+    }
+}
